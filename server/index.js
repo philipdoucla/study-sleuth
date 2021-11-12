bb61aea450a0036df49cd7e0c92bc2245988454f
James Shiffer <jamesnshiffer@gmail.com>
2021-11-12 01:08:21 -0800

    Added login and register features
    See /login and /register API endpoints. Also added relevant database migrations and Sequelize models.

--- server/index.js
+++ server/index.js
@@ -1,57 +1,118 @@
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
 const express = require('express');
 const passport = require('passport');
-const bcrypt = require('bcrypt');
-const mysql = require('mysql')
-const dotenv = require('dotenv');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/User.js');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
 
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+}, async (email, password, done) => {
+    try {
+        const user = await User.findOne({
+            where: { email }
+        });
+        if (!user) {
+            return done(null, false, { error: 'A user with that email does not exist.' });
+        }
+        const correctPassword = await bcrypt.compare(password, user.password);
+        if (!correctPassword) {
+            return done(null, false, { error: 'Incorrect password.' });
+        }
+        return done(null, user);
+    } catch (err) {
+        return done(err);
+    }
+}));
 
-dotenv.config({path : './.env.local'});
-
-const db =   mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-}); 
-
-db.connect( (error) => {
-    if(error) throw error;
-    console.log("Connection Secured")
+passport.serializeUser((id, done) => {
+    done(null, id);
 });
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findOne({ where: { id } });
+        done(null, user);
+    } catch (err) {
+        done(err, null);
+    }
+});
+
 
 app.get('/', (req, res) => {
     res.send('hello world');
 });
 
-app.post('/register', (req, res) => {
-    const {username, email, password, passwordConfirm} = req.body;
-    
-    db.query('SELECT email FROM users WHERE email = ?', [email], async (error, results) => {
-        if(error) throw error;
+app.post('/login', passport.authenticate('local'),
+    async (req, res) => {
+        // this function is only reached if login succeeded
+        res.status(200).json(req.user);
+    }
+);
 
-        if (res.length > 0) {
-            return res.render('register', {
-                message: 'Email in Use'
-            });
-        } else if (password != passwordConfirm) {
-            return res.render('register', {
-                messsage : "Passwords do not match"
-            });
-        }
+app.post('/register', async (req, res) => {
+    const { email, password, passwordConfirm, firstName, lastName } = req.body;
 
-        const hashedPassword = await bycrypt.hash(password, 10)
-        
-        db.query('INSERT INTO users SET ?', {name: username, email: email, password: hashedPassword}, () => {
-            if (error) throw error; 
-            res.render('register', {
-                message : 'User successfully registered'
-            });
+    if (!email) {
+        // TODO: check that email has ucla.edu domain
+        return res.status(422).json({
+            error: 'Email address must be provided'
         });
-    });
+    }
+
+    let user;
+    try {
+        user = await User.findOne({
+            where: { email }
+        });
+    } catch (error) {
+        return res.status(500).json({ error });
+    }
+
+    if (user) {
+        return res.status(422).json({
+            error: 'Email in use'
+        });
+    }
+
+    if (password !== passwordConfirm) {
+        return res.status(422).json({
+            error: 'Passwords do not match'
+        });
+    }
+
+    if (password.length < 6) {
+        return res.status(422).json({
+            error: 'Password should be at least 6 characters in length'
+        });
+    }
+
+    if (!firstName || !lastName) {
+        return res.status(422).json({
+            error: 'First and last name must be provided'
+        });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+        const newUser = await User.create({
+            id: User.randomId(),
+            email,
+            password: hashedPassword,
+            firstName,
+            lastName
+        });
+        return res.status(200).json(newUser.toJSON());
+    } catch (error) {
+        return res.status(500).json({ error });
+    }
 })
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
