d2b5e54d29025d7ac9f9b61b4f9dd9432ef6da45
Derek Lee <derekclee323@g.ucla.edu>
2021-11-30 21:58:25 -0800

    Add Stars
    Pushing to let Vinh work on it

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -1,12 +1,37 @@
 import { Link, Redirect } from 'react-router-dom';
-
+import ReactStars from 'react-rating-stars-component'
+import React, { useState } from 'react';
 const Dashboard = ({loggedIn}) => {
     // TODO: needs to actually display data and junk
     // the findgroup login must save account login
 
+    const [showRating, setShow] = useState(false);
+
     if(!loggedIn) {
         return (<Redirect to="login"/>)
     }
+
+    const updateRating = (newRating) => {
+        console.log("Hi: " + newRating);
+    }
+
+    const groupMember = (memberName, showRating) => {
+        return (
+            <li>{memberName}{showRating ? <ReactStars count={5} onChange={updateRating} size={24} activeColor="#ffd700"/> : null}</li>
+        )
+    }
+
+    const ratingButton = () => {
+        if(!showRating) {
+            return (<button type="button" className="switch" onClick={() => setShow(true)}>Rate Your Group</button>)
+        }
+        return (<button type="button" className="switch" onClick={() => {setShow(false); saveRatings()}}>Save</button>)
+    }
+
+    const saveRatings = () => {
+        console.log("hi");
+    }
+
 
     return (
         // will need to display group data dynamically
@@ -15,15 +40,15 @@ const Dashboard = ({loggedIn}) => {
             <div className="GroupTitle">Your Study Sleuth:</div>
             <div>
                 <ul className="grouplist">
-                    <li>Philip Do</li>
-                    <li>Derek Le</li>
-                    <li>Vinh Nguyen</li>
-                    <li>James Shiffer</li>
-                    <li>Dhaval Vora</li>
+                    {groupMember("Philip Do", showRating)}
+                    {groupMember("Derek Lee", showRating)}
+                    {groupMember("James Shiffer", showRating)}
+                    {groupMember("Vinh Ngyuen", showRating)}
+                    {groupMember("Dhaval Vora", showRating)}
                 </ul>
             </div>
             <br />
-            <div><button type="button" className="switch">Rate Your Group</button></div>
+            <div>{ratingButton()}</div>
             <br />
             <div><Link to="/findgroup" className="switch">Find New Group</Link></div>
         </div>
